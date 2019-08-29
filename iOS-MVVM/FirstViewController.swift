9ffccc9f1da9fb7ac3290456489f3a2df7ffd54b
Marcin Maćkowiak <marcin@mmackowiak.com>
2019-08-29 13:11:20 +0200

    Initial Commit

---
+++ iOS-MVVM/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iOS-MVVM
+//
+//  Created by Marcin Maćkowiak on 29/08/2019.
+//  Copyright © 2019 MeWe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-MVVM/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iOS-MVVM
+//
+//  Created by Marcin Maćkowiak on 29/08/2019.
+//  Copyright © 2019 MeWe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
